5916f508a55c8541b385948ff8c4984b1881a25d
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-04-26 01:34:35 -0300

    changed version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cssgen "0.1.0-SNAPSHOT"
+(defproject cssgen "0.1.0"
   :description "Generate CSS from clj code. Like an embedded sass."
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
