9a56c6f8e11efaf3397fb72b7132bfb52da0c510
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-04-26 02:03:24 -0300

    added url to project

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject cssgen "0.1.0"
   :description "Generate CSS from clj code. Like an embedded sass."
+  :url "http://github.com/paraseba/cssgen"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
