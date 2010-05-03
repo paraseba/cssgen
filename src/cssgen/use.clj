27e4702bbe22c451fbb8711f42cfdba95f714b46
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-05-03 00:59:34 -0300

    added macro to hepl with (ns) declaration

---
+++ src/cssgen/use.clj
@@ -0,0 +1,9 @@
+(ns cssgen.use)
+
+(defmacro css-ns [ns-name & forms]
+  `(ns ~ns-name
+     (:refer-clojure :exclude [~'+ ~'- ~'* ~'/])
+     (:use
+       ~'cssgen ~'cssgen.types
+       [clojure.contrib.generic.arithmetic :only (~'+ ~'- ~'* ~'/)])
+     ~@forms))
