ab4f835136aa2c72b9f07fcea1b8822e10342652
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-05-04 02:40:30 -0300

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cssgen "0.2.0"
+(defproject cssgen "0.2.1"
   :description "Generate CSS from clojure code. Like an embedded sass."
   :url "http://github.com/paraseba/cssgen"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
