7d7bdd5894633bf31c22be06503fd9271966aafe
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-05-03 21:46:38 -0300

    simplified macro syntax: css-ns

--- src/cssgen/use.clj
+++ src/cssgen/use.clj
@@ -2,8 +2,8 @@
 
 (defmacro css-ns [ns-name & forms]
   `(ns ~ns-name
-     (:refer-clojure :exclude [~'+ ~'- ~'* ~'/])
-     (:use
-       ~'cssgen ~'cssgen.types
-       [clojure.contrib.generic.arithmetic :only (~'+ ~'- ~'* ~'/)])
+     ~'(:refer-clojure :exclude [+ - * /])
+     ~'(:use
+         cssgen cssgen.types
+         [clojure.contrib.generic.arithmetic :only (+ - * /)])
      ~@forms))
