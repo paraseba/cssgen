d9d4af0398cd9de6417e4942aefd053e01028ce1
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2010-11-15 19:52:11 -0200

    changed clojure and c.c. to 1.2.0

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject cssgen "0.2.1"
+(defproject cssgen "0.2.2"
   :description "Generate CSS from clojure code. Like an embedded sass."
   :url "http://github.com/paraseba/cssgen"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[vimclojure/server "2.2.0"]])
