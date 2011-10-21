825c8de5c74b8ea2efff40c9f0b26271610ee8d0
technomancy <technomancy@ip-10-244-9-182.ec2.internal>
2011-10-21 19:18:35 +0000

    Support both Clojure 1.2 and 1.3 via lein-multi.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject cssgen "0.2.5-SNAPSHOT"
   :description "Generate CSS from clojure code. Like an embedded sass."
   :url "http://github.com/paraseba/cssgen"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
                  [org.clojure/algo.generic "0.1.0-SNAPSHOT"]]
-  :dev-dependencies [[vimclojure/server "2.2.0"]])
+  :dev-dependencies [[lein-multi "1.0.0"]]
+  :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
+                      [org.clojure/algo.generic "0.1.0-SNAPSHOT"]]}
+  :repositories {"sonatype" "http://oss.sonatype.org/content/repositories/snapshots"})
