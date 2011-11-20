f34533573c9fdb5011967db2c99ceab60de811d1
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2011-11-20 16:18:22 -0200

    0.2.5 release

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject cssgen "0.2.5-SNAPSHOT"
+(defproject cssgen "0.2.5"
   :description "Generate CSS from clojure code. Like an embedded sass."
   :url "http://github.com/paraseba/cssgen"
   :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
-                 [org.clojure/algo.generic "0.1.0-SNAPSHOT"]]
+                 [org.clojure/algo.generic "0.1.0"]]
   :dev-dependencies [[lein-multi "1.0.0"]]
   :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
-                      [org.clojure/algo.generic "0.1.0-SNAPSHOT"]]}
-  :repositories {"sonatype" "http://oss.sonatype.org/content/repositories/snapshots"})
+                      [org.clojure/algo.generic "0.1.0"]]})
