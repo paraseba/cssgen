6c0ee2e3f9a946595560e670373dd4e180c3c4c6
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2011-11-29 00:13:49 -0200

    using multi-deps

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject cssgen "0.3.0-SNAPSHOT"
   :description "Generate CSS from clojure code. Like an embedded sass."
   :url "http://github.com/paraseba/cssgen"
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+  :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
+                 [org.clojure/algo.generic "0.1.0"]
                  [org.clojure/core.incubator "0.1.0"]]
-  :dev-dependencies [[test-this "0.2.3"]])
+  :dev-dependencies [[lein-multi "1.0.0"]
+                     [test-this "0.2.3"]]
+  :multi-deps {"1.2" [[org.clojure/clojure "1.2.0"]
+                      [org.clojure/algo.generic "0.1.0"]]})
+
