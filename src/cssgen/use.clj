89188c35700c9e309dff5de84301810b1ad02967
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2011-12-06 16:24:11 -0200

    fix css-ns macro

--- src/cssgen/use.clj
+++ src/cssgen/use.clj
@@ -3,7 +3,6 @@
 (defmacro css-ns [ns-name & forms]
   `(ns ~ns-name
      ~'(:refer-clojure :exclude [+ - * /])
-     ~'(:use
-         cssgen cssgen.types
-         [clojure.contrib.generic.arithmetic :only (+ - * /)])
+     ~'(:use cssgen cssgen.types
+             [clojure.algo.generic.arithmetic :only (+ - * /)])
      ~@forms))
