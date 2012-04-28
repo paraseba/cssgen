fe9c751adc88b0218300582e984fa60f935e140f
Sebastián Bernardo Galkin <paraseba@gmail.com>
2012-04-28 15:02:49 -0300

    trying travis

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github.com/paraseba/cssgen"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/algo.generic "0.1.0"]]
-  :dev-dependencies [#_[lein-multi "1.0.0"]
-                     #_[test-this "0.2.3"]]
+  :dev-dependencies [[lein-multi "1.0.0"]
+                     [test-this "0.2.3"]]
   :multi-deps {"1.2" [[org.clojure/clojure "1.2.0"]
                       [org.clojure/algo.generic "0.1.0"]]
                ;"1.3" [[org.clojure/clojure "1.3.0"]
