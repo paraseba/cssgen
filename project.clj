55853a5a9baaa3532b64d7de9caa9e09da321fc2
Sebastián Bernardo Galkin <paraseba@gmail.com>
2012-04-28 15:22:35 -0300

    enable travis test

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject cssgen "0.2.6"
   :description "Generate CSS from clojure code. Like an embedded sass."
   :url "http://github.com/paraseba/cssgen"
-  :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/algo.generic "0.1.0"]]
   :dev-dependencies [[lein-multi "1.0.0"]]
-  :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
+  :multi-deps {"1.4" [[org.clojure/clojure "1.4.0"]
+                      [org.clojure/algo.generic "0.1.0"]]
+               "1.3" [[org.clojure/clojure "1.3.0"]
                       [org.clojure/algo.generic "0.1.0"]]})
+
