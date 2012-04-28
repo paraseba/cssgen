25504e6895fdba432c2e751fc190a203b05dfcc7
Sebastián Bernardo Galkin <paraseba@gmail.com>
2012-04-28 14:52:08 -0300

    try travis without clj 1.3.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cssgen "0.3.0-alpha1"
   :description "Generate CSS from clojure code. Like an embedded sass."
   :url "http://github.com/paraseba/cssgen"
-  :dependencies [[org.clojure/clojure "[1.2.0,1.4.0]"]
+  :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
                  [org.clojure/algo.generic "0.1.0"]]
   :dev-dependencies [[lein-multi "1.0.0"]
                      [test-this "0.2.3"]]
