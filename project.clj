12a54e65f93d20523054aac819d5cf7a1e5cdaae
Sebastián Bernardo Galkin <paraseba@gmail.com>
2012-04-28 15:00:24 -0300

    try trevis with clj 1.2.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject cssgen "0.3.0-alpha1"
   :description "Generate CSS from clojure code. Like an embedded sass."
   :url "http://github.com/paraseba/cssgen"
-  :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/algo.generic "0.1.0"]]
-  :dev-dependencies [[lein-multi "1.0.0"]
-                     [test-this "0.2.3"]]
+  :dev-dependencies [#_[lein-multi "1.0.0"]
+                     #_[test-this "0.2.3"]]
   :multi-deps {"1.2" [[org.clojure/clojure "1.2.0"]
                       [org.clojure/algo.generic "0.1.0"]]
                ;"1.3" [[org.clojure/clojure "1.3.0"]
